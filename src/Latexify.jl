9f8c71e86a4d179e4cad611c473a8db3c4239aac
Niklas Korsbo <niklas.korsbo@gmail.com>
2017-10-21 21:09:16 +0100

    Hotfix: export latexarray.

--- src/Latexify.jl
+++ src/Latexify.jl
@@ -1,8 +1,9 @@
 __precompile__()
 module Latexify
 import DiffEqBase, SymEngine
-export latexify
+export latexify, latexarray
 
 include("latexify_function.jl")
 include("latexoperation.jl")
+include("latexenv.jl")
 end
