4d47ac32950760f2e1824b4dab28de84a09eee41
Niklas Korsbo <niklas.korsbo@gmail.com>
2017-10-22 13:08:21 +0100

    Add support for Documenter.jl

---
+++ docs/make.jl
@@ -0,0 +1,13 @@
+using Documenter
+using Latexify
+
+makedocs(
+    modules = [Latexify]
+)
+
+# Documenter can also automatically deploy documentation to gh-pages.
+# See "Hosting Documentation" and deploydocs() in the Documenter manual
+# for more information.
+#=deploydocs(
+    repo = "<repository url>"
+)=#
---
+++ docs/src/index.md
@@ -0,0 +1,27 @@
+# Latexify.jl
+
+Documentation for Latexify.jl
+
+
+```@contents
+```
+
+## Functions
+
+
+```@meta
+DocTestSetup = quote
+using Latexify
+end
+```
+```@docs
+latexify
+latexarray
+```
+```@meta
+DocTestSetup = nothing
+```
+
+## Index
+```@index
+```
