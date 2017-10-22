2845603f1e8e74fe029b378af324bf0062234a03
Niklas Korsbo <niklas.korsbo@gmail.com>
2017-10-22 16:39:28 +0100

    wip: trying to get document pages working.

--- docs/make.jl
+++ docs/make.jl
@@ -3,14 +3,19 @@ using Latexify
 
 makedocs(
     modules = [Latexify],
-    doctext = true
+    doctest = true
 )
 
-deploydocs(deps   = Deps.pip("mkdocs", "python-markdown-math"),
+deploydocs(
+    deps = Deps.pip("mkdocs", "python-markdown-math"),
     repo = "github.com/korsbo/Latexify.jl.git",
-    julia  = "0.6",
+    branch = "gh-pages",
+    julia  = "nightly",
     target = "build",
-    osname = "linux")
+    osname = "linux"
+    )
+
+
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
