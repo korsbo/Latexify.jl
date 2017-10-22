ebfa2e99ece97d1008881f65af23367f2afe77ec
Niklas Korsbo <niklas.korsbo@gmail.com>
2017-10-22 15:09:19 +0100

    wip: trying to set up document pages.

--- docs/make.jl
+++ docs/make.jl
@@ -9,7 +9,8 @@ makedocs(
 deploydocs(deps   = Deps.pip("mkdocs", "python-markdown-math"),
     repo = "github.com/korsbo/Latexify.jl.git",
     julia  = "0.6",
-    osname = "osx")
+    target = "build",
+    osname = "linux")
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
