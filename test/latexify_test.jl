b2f95873962b744fc62cfc68aed358f2330aa14d
Niklas Korsbo <niklas.korsbo@gmail.com>
2017-11-12 21:39:31 +0000

    Improve test coverage.

--- test/latexify_test.jl
+++ test/latexify_test.jl
@@ -16,6 +16,20 @@ push!(test_results, latexify(:y_c_a) == "y_{c\\_a}")
 push!(test_results, latexify(1.0) == "1.0")
 push!(test_results, latexify(1.00) == "1.0")
 push!(test_results, latexify(1) == "1")
+push!(test_results, latexify(:(log10(x))) == "\\log_{10}\\left( x \\right)")
+push!(test_results, latexify(:(sin(x))) ==  "\\sin\\left( x \\right)")
+push!(test_results, latexify("x = 4*y") == "x = 4 \\cdot y")
+push!(test_results, latexify(:(sqrt(x))) == "\\sqrt{x}")
+
+
 
 push!(test_results, latexify(1//2) == "\\frac{1}{2}")
+
+using DifferentialEquations
+f = @ode_def feedback begin
+    dx = y/c_1 - x
+    dy = x^c_2 - y
+end c_1=>1.0 c_2=>1.0
+push!(test_results, latexify(f) == ["dx/dt = \\frac{y}{c_{1}} - x", "dy/dt = x^{c_{2}} - y"])
+
 all(test_results)
