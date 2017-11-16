66b755da4ebdb36cefc20496de2a1903e955019f
Niklas Korsbo <niklas.korsbo@slcu.cam.ac.uk>
2017-11-16 14:54:02 +0000

    wip: trying to deploy docs.

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,7 @@ using Latexify
 
 makedocs(
     modules = [Latexify],
-    format = :html,
+    #format = :html,
     sitename = "Latexify.jl",
     doctest = false
 )
@@ -11,7 +11,7 @@ makedocs(
 deploydocs(
     deps = Deps.pip("mkdocs", "python-markdown-math"),
     repo = "github.com/korsbo/Latexify.jl.git",
-    julia  = "0.6",
+    julia  = "release",
     target = "build",
     osname = "linux"
     )
