64fe43197ecc42dd67cf1fd78a159a09c6cc8e90
Niklas Korsbo <niklas.korsbo@slcu.cam.ac.uk>
2017-11-16 16:28:59 +0000

    Hopefully fixes doc deployment.

--- docs/make.jl
+++ docs/make.jl
@@ -5,15 +5,30 @@ makedocs(
     modules = [Latexify],
     format = :html,
     sitename = "Latexify.jl",
+    pages = [
+        "index.md",
+        "Demos" => [
+            "tutorials/latexify.md",
+            "tutorials/latexarray.md"
+        ],
+        "Functions" => [
+            "functions/latexify.md",
+            "functions/latexalign.md",
+            "functions/latexarray.md",
+            "functions/latexoperation.md"
+        ]
+    ],
     doctest = false
 )
 
 deploydocs(
-    deps = Deps.pip("mkdocs", "python-markdown-math"),
+    #deps = Deps.pip("mkdocs", "python-markdown-math"),
     repo = "github.com/korsbo/Latexify.jl.git",
     julia  = "release",
     target = "build",
-    osname = "linux"
+    osname = "linux",
+    make = nothing,
+    deps = nothing
     )
 
 
