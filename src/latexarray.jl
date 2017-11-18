d007e40f85091a217d0d6a33c5f9fad4f3558047
Niklas Korsbo <niklas.korsbo@gmail.com>
2017-11-18 15:05:50 +0000

    Add latexarray support for vectors.

--- src/latexarray.jl
+++ src/latexarray.jl
@@ -27,3 +27,21 @@ function latexarray{T}(arr::AbstractArray{T, 2}; adjustment::Symbol=:c)
     str *= "\\right]\n"
     str *= "\\end{equation}\n"
 end
+
+function latexarray{T}(arr::AbstractArray{T, 1}; adjustment::Symbol=:c)
+    rows = length(arr)
+    columns = (1)
+    str = "\\begin{equation}\n"
+    str *= "\\left[\n"
+    str *= "\\begin{array}{" * "$(adjustment)"^columns * "}\n"
+
+    isa(arr, Matrix{String}) || (arr = latexify(arr))
+    for i=1:rows, j=1:columns
+        str *= arr[i,j]
+        j==columns ? (str *= "\\\\ \n") : (str *= " & ")
+    end
+
+    str *= "\\end{array}\n"
+    str *= "\\right]\n"
+    str *= "\\end{equation}\n"
+end
