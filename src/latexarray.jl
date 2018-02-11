d9f581852d60badfc43f347779634c6790be6151
Niklas Korsbo <niklas.korsbo@gmail.com>
2018-02-11 16:23:17 +0000

    Neaten up implementation of latexarray by using recursion and overloads.

--- src/latexarray.jl
+++ src/latexarray.jl
@@ -12,13 +12,14 @@ latexarray(arr)
 "\\begin{equation}\n\\left[\n\\begin{array}{cc}\n1 & 2\\\\ \n3 & 4\\\\ \n\\end{array}\n\\right]\n\\end{equation}\n"
 ```
 """
-function latexarray{T}(arr::AbstractArray{T, 2}; adjustment::Symbol=:c)
+function latexarray(arr::AbstractMatrix; adjustment::Symbol=:c, transpose=false)
+    transpose && (arr = permutedims(arr, [2,1]))
     (rows, columns) = size(arr)
     str = "\\begin{equation}\n"
     str *= "\\left[\n"
     str *= "\\begin{array}{" * "$(adjustment)"^columns * "}\n"
 
-    T == LaTeXStrings.LaTeXString || (arr = latexraw(arr))
+    arr = latexraw(arr)
     for i=1:rows, j=1:columns
         str *= arr[i,j]
         j==columns ? (str *= "\\\\ \n") : (str *= " & ")
@@ -32,23 +33,5 @@ function latexarray{T}(arr::AbstractArray{T, 2}; adjustment::Symbol=:c)
     return latexstr
 end
 
-function latexarray{T}(arr::AbstractArray{T, 1}; adjustment::Symbol=:c, transpose=false)
-    rows = length(arr)
-    str = "\\begin{equation}\n"
-    str *= "\\left[\n"
-    str *= "\\begin{array}{" * "$(adjustment)" * "}\n"
 
-    T == LaTeXStrings.LaTeXString || (arr = latexraw(arr))
-    for i=1:rows
-        str *= arr[i]
-        transpose ?  i != rows && (str *= " & ") : (str *= "\\\\ \n")
-    end
-    transpose && (str *= " \n")
-
-    str *= "\\end{array}\n"
-    str *= "\\right]\n"
-    str *= "\\end{equation}\n"
-    latexstr = LaTeXString(str)
-    COPY_TO_CLIPBOARD && clipboard(latexstr)
-    return latexstr
-end
+latexarray(vec::AbstractVector; kwargs...) = latexarray(hcat(vec); kwargs...)
