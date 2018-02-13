d3620e327ab29d7c0bc479d4dd5c6ac3782aa4f5
autodocs <autodocs>
2018-02-13 22:26:48 +0000

    build based on e138ec8

--- latest/search_index.js
+++ latest/search_index.js
@@ -153,6 +153,22 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
+    "location": "tutorials/DiffEqBiological.html#",
+    "page": "Use with @reaction_network from DiffEqBiological.jl.",
+    "title": "Use with @reaction_network from DiffEqBiological.jl.",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "tutorials/DiffEqBiological.html#Use-with-@reaction_network-from-DiffEqBiological.jl.-1",
+    "page": "Use with @reaction_network from DiffEqBiological.jl.",
+    "title": "Use with @reaction_network from DiffEqBiological.jl.",
+    "category": "section",
+    "text": "Latexify.jl has methods for dealing with AbstractReactionNetworks. For more information regarding this DSL, turn to its docs. The latexify end of things are pretty simple: feed a reaction network to the latexify or latexalign functions (they do the same thing in this case) and let them do their magic.using DifferentialEquations\nusing Latexify\ncopy_to_clipboard(true)\n\n@reaction_func hill2(x, v, k) = v*x^2/(k^2 + x^2)\nrn = @reaction_network MyRnType begin\n  hill2(y, v_x, k_x), 0 --> x\n  p_y, 0 --> y\n  (d_x, d_y), (x, y) --> 0\n  (r_b, r_u), x ↔ y\nend v_x k_x p_y d_x d_y r_b r_u\n\nlatexify(rn)$\\begin{align} \\frac{dx}{dt} =& - x \\cdot d_{x} - x \\cdot r_{b} + y \\cdot r_{u} + \\frac{y^{2} \\cdot v_{x}}{k_{x}^{2} + y^{2}} \\\n\\frac{dy}{dt} =& p_{y} + x \\cdot r_{b} - y \\cdot d_{y} - y \\cdot r_{u} \\\n\\end{align} $The method has a keyword for choosing between outputting the ODE or the noise term. While it is not obvious from the latexify output, the noise in the reaction network is correlated.latexify(rn; noise=true)$\\begin{align} \\frac{dx}{dt} =& - \\sqrt{x \\cdot d_{x}} - \\sqrt{x \\cdot r_{b}} + \\sqrt{y \\cdot r_{u}} + \\sqrt{\\frac{y^{2} \\cdot v_{x}}{k_{x}^{2} + y^{2}}} \\\n\\frac{dy}{dt} =& \\sqrt{p_{y}} + \\sqrt{x \\cdot r_{b}} - \\sqrt{y \\cdot d_{y}} - \\sqrt{y \\cdot r_{u}} \\\n\\end{align} $SymEngine will be used by default to clean up the expressions. A disadvantage of this is that the order of the terms and the operations within the terms becomes unpredictable. You can therefore turn this symbolic magic off. The result has some ugly issues with + -1, but the option is there, and here is how to use it:  latexify(rn; symbolic=false)$\\begin{align} \\frac{dx}{dt} =& \\frac{v_{x} \\cdot y^{2}}{k_{x}^{2} + y^{2}} + -1 \\cdot d_{x} \\cdot x + -1 \\cdot r_{b} \\cdot x + r_{u} \\cdot y \\\n\\frac{dy}{dt} =& p_{y} + -1 \\cdot d_{y} \\cdot y + r_{b} \\cdot x + -1 \\cdot r_{u} \\cdot y \\\n\\end{align} $latexify(rn; noise=true, symbolic=false)$\\begin{align} \\frac{dx}{dt} =& \\sqrt{\\frac{v_{x} \\cdot y^{2}}{k_{x}^{2} + y^{2}}} + - \\sqrt{d_{x} \\cdot x} + - \\sqrt{r_{b} \\cdot x} + \\sqrt{r_{u} \\cdot y} \\\n\\frac{dy}{dt} =& \\sqrt{p_{y}} + - \\sqrt{d_{y} \\cdot y} + \\sqrt{r_{b} \\cdot x} + - \\sqrt{r_{u} \\cdot y} \\\n\\end{align} $"
+},
+
+{
     "location": "tutorials/inner_workings.html#",
     "page": "Inner workings",
     "title": "Inner workings",
