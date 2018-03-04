d3f6ea37a1441885409696647d9ddd019d93b55b
Niklas Korsbo <niklas.korsbo@gmail.com>
2018-03-04 22:23:25 +0000

    Add mdtable docstring.

--- src/mdtable.jl
+++ src/mdtable.jl
@@ -1,3 +1,60 @@
+doc"""
+    mdtable(array; latex=true, head=[], side=[], transpose=false)
+
+Latexify the input and output a markdown-formatted table.
+
+```julia
+julia> using Latexify
+juila> copy_to_clipboard(true)
+julia> M = ["x/y" 1//2
+            "p_m" "e^2"]
+julia> mdtable(M)
+```
+
+| $\frac{x}{y}$ | $\frac{1}{2}$ |
+| -------------:| -------------:|
+|       $p_{m}$ |       $e^{2}$ |
+
+```julia
+julia> head = ["Column 1", "Column 2"]
+julia> side = ["Row 1", "Row 2"]
+julia> mdtable(M; head=head, side=side)
+```
+
+|     . |      Column 1 |      Column 2 |
+| -----:| -------------:| -------------:|
+| Row 1 | $\frac{x}{y}$ | $\frac{1}{2}$ |
+| Row 2 |       $p_{m}$ |       $e^{2}$ |
+
+
+The value in the top right corner can be set if you let the `side` vector be one element
+larger than the number of rows of your input:
+
+```julia
+julia> side = ["Corner", "Row 1", "Row 2"]
+julia> mdtable(M; head=head, side=side)
+```
+
+| Corner |      Column 1 |      Column 2 |
+| ------:| -------------:| -------------:|
+|  Row 1 | $\frac{x}{y}$ | $\frac{1}{2}$ |
+|  Row 2 |       $p_{m}$ |       $e^{2}$ |
+
+
+The `head` and `side` vectors are not latexifed, but you can easily do this yourself:
+
+```julia
+julia> head = ["p_1", "p_2"]
+julia> mdtable(M; head=latexinline(head))
+```
+
+|       $p_{1}$ |       $p_{2}$ |
+| -------------:| -------------:|
+| $\frac{x}{y}$ | $\frac{1}{2}$ |
+|       $p_{m}$ |       $e^{2}$ |
+"""
+function mdtable() end
+
 function mdtable(M::AbstractMatrix; latex::Bool=true, head=[], side=[], transpose=false)
     transpose && (M = permutedims(M, [2,1]))
     latex && (M = latexinline(M))
