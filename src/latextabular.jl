3b6a61e3262d6d9d7ff87a08985aca818731df49
Niklas Korsbo <niklas.korsbo@slcu.cam.ac.uk>
2018-07-16 13:32:28 +0100

    Improve latextabular. More kwargs and DataFrames.

--- src/latextabular.jl
+++ src/latextabular.jl
@@ -1,14 +1,25 @@
 
 
-function latextabular(arr::AbstractMatrix; adjustment::Symbol=:c, transpose=false)
+function latextabular(arr::AbstractMatrix; latex::Bool=true, head=[], side=[], adjustment::Symbol=:c, transpose=false)
     transpose && (arr = permutedims(arr, [2,1]))
+
+    if !isempty(head)
+        arr = vcat(hcat(head...), arr)
+        @assert length(head) == size(arr, 2) "The length of the head does not match the shape of the input matrix."
+    end
+    if !isempty(side)
+        length(side) == size(arr, 1) - 1 && (side = ["."; side]) ## why is empty not allowed?
+        @assert length(side) == size(arr, 1) "The length of the side does not match the shape of the input matrix."
+        arr = hcat(side, arr)
+    end
+
     (rows, columns) = size(arr)
     str = "\\begin{tabular}{" * "$(adjustment)"^columns * "}\n"
 
-    arr = latexinline(arr)
-    for i=1:rows, j=1:columns
-        str *= arr[i,j]
-        j==columns ? (str *= "\\\\ \n") : (str *= " & ")
+    arr =  latex ? latexinline(arr) : string.(arr)
+    for i in 1:size(arr, 1)
+        str *= join(arr[i,:], " & ")
+        str *= "\\\\\n"
     end
 
     str *= "\\end{tabular}\n"
@@ -20,3 +31,7 @@ end
 
 latextabular(vec::AbstractVector; kwargs...) = latextabular(hcat(vec...); kwargs...)
 latextabular(vectors::AbstractVector...; kwargs...) = latextabular(hcat(vectors...); kwargs...)
+
+@require DataFrames begin
+    latextabular(d::DataFrames.DataFrame; kwargs...) =latextabular(hcat(d.columns...); head=keys(d.colindex), kwargs...)
+end
