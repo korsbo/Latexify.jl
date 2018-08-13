43634d97a3d82c4fd44270a9cb8c84f1f4690e86
autodocs <autodocs>
2018-08-13 18:54:49 +0000

    build based on 6942ada

--- versions.js
+++ versions.js
@@ -1,11 +1,13 @@
 var DOC_VERSIONS = [
   "stable",
   "latest",
+  "release-0.5",
   "release-0.4",
   "release-0.3",
   "release-0.2",
   "release-0.1",
   "release-0.0",
+  "v0.5.0",
   "v0.4.1",
   "v0.4.0",
   "v0.3.0",
