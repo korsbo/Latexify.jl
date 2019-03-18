830f04d3753993b452efdf5149a45d8fd153d3a8
autodocs <autodocs>
2019-03-18 14:52:23 +0000

    build based on 18db846

--- versions.js
+++ versions.js
@@ -1,6 +1,7 @@
 var DOC_VERSIONS = [
   "stable",
   "latest",
+  "release-0.8",
   "release-0.7",
   "release-0.6",
   "release-0.5",
@@ -9,6 +10,7 @@ var DOC_VERSIONS = [
   "release-0.2",
   "release-0.1",
   "release-0.0",
+  "v0.8.0",
   "v0.7.0",
   "v0.6.0",
   "v0.5.1",
