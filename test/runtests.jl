65fdd1394eb929fe69f1da176c1faafd1a17c735
Simeon Schaub <simeondavidschaub99@gmail.com>
2019-04-03 18:58:12 +0200

    added some tests for numberformatters

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,4 +18,5 @@ using Test
 @testset "mdtable tests" begin include("mdtable_test.jl") end
 @testset "chemical_arrows test" begin include("chemical_arrows_test.jl") end
 @testset "DataFrame Plugin" begin include("plugins/DataFrames.jl") end
-@testset "unocode2latex" begin include("unicode2latex.jl") end
+@testset "unicode2latex" begin include("unicode2latex.jl") end
+@testset "numberformatters" begin include("numberformatters_test.jl") end
