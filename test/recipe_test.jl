1ba265e0a2ef357ecc280c2d8b27a910854be0f6
Niklas Korsbo <niklas.korsbo@slcu.cam.ac.uk>
2019-09-27 15:05:26 +0100

    Fix recipe tests.

--- test/recipe_test.jl
+++ test/recipe_test.jl
@@ -19,54 +19,55 @@ end
 
 using .MyModule
 t = MyModule.MyType([:A, :B, 3], [1, 2, 3])
-t2 = MyModule.MyType([:X, :Y, :(x/y)], [1.23434534, 232423.42345, 12//33])
+t2 = MyModule.MyType([:X, :Y, :(x/y)], Number[1.23434534, 232423.42345, 12//33])
+
 
 @test latexify(t2, fmt="%.8f") == 
 raw"\begin{align}
 X =& 1.23 \\
 Y =& 232423.42 \\
-\frac{x}{y} =& 0.36
+\frac{x}{y} =& \frac{4}{11}
 \end{align}
 "
 
-
 @test latexify(t) == 
 raw"\begin{align}
-A =& 1 \\
-B =& 2 \\
-3 =& 3
+A =& 1.00 \\
+B =& 2.00 \\
+3.00 =& 3.00
 \end{align}
 "
 
 @test latexify(t; reverse=true) == 
 raw"\begin{align}
-1 =& A \\
-2 =& B \\
-3 =& 3
+1.00 =& A \\
+2.00 =& B \\
+3.00 =& 3.00
 \end{align}
 "
 
-@test latexify(t, env=:array) == 
+@test latexify(t; env=:array) == 
 raw"\begin{equation}
 \left[
 \begin{array}{cc}
-A & 1 \\
-B & 2 \\
-3 & 3 \\
+A & 1.00 \\
+B & 2.00 \\
+3.00 & 3.00 \\
 \end{array}
 \right]
 \end{equation}
 "
 
-@test latexify(t, env=:array, reverse=true) == 
+@test latexify(t; env=:array, reverse=true) == 
 raw"\begin{equation}
 \left[
 \begin{array}{cc}
-1 & A \\
-2 & B \\
-3 & 3 \\
+1.00 & A \\
+2.00 & B \\
+3.00 & 3.00 \\
 \end{array}
 \right]
 \end{equation}
 "
+
 
