d4323f40835fad5376446158c0a1e06ff962eec4
Robert Moss <rmoss92@gmail.com>
2020-09-13 14:57:59 -0700

    Passing tests and changed "word" to > 2 characters (think dx/dy)

--- src/latexoperation.jl
+++ src/latexoperation.jl
@@ -11,8 +11,8 @@ function latexoperation(ex::Expr, prevOp::AbstractArray; cdot=true, kwargs...)
     convertSubscript!(ex)
     args = map(i -> i isa Number ? latexraw(i; kwargs...) : i, ex.args)
 
-    # Remove math italics for variables (i.e. words) longer than 1 character.
-    args = map(i -> (i isa String && all(map(isletter, collect(i))) && length(i) > 1) ? "{\\rm $i}" : i, args)
+    # Remove math italics for variables (i.e. words) longer than 2 characters.
+    args = map(i -> (i isa String && all(map(isletter, collect(i))) && length(i) > 2) ? "{\\rm $i}" : i, args)
 
 
     if op in [:/, :./]
