c8fd6deeb29a3ee1e7b31879b0955cba1e9a36bb
Niklas Korsbo <niklas.korsbo@slcu.cam.ac.uk>
2020-11-13 11:20:41 +0100

    Add test for array of separators to align.

--- test/latexalign_test.jl
+++ test/latexalign_test.jl
@@ -9,5 +9,13 @@ raw"\begin{align}
 \end{align}
 "
 
+@test latexify(((1.0, 2), (3, 4)); separator = [" =& ", " ∈& "], env = :align) == 
+raw"\begin{align}
+1.0 =& 3 \\
+2 ∈& 4
+\end{align}
+"
+
+
 
 # @test_throws MethodError latexify(rn; bad_kwarg="should error")
