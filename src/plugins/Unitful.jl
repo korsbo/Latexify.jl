aaf6d239b6c204eb8250879f53aa0f1e59a3f176
David Gustavsson <david.e.gustavsson@gmail.com>
2020-12-07 17:36:16 +0100

    Latexrecipize

--- src/plugins/Unitful.jl
+++ src/plugins/Unitful.jl
@@ -1,82 +1,101 @@
-const mathrmprefixes = Dict(
-                            -24 => "y",
-                            -21 => "z",
-                            -18 => "a",
-                            -15 => "f",
-                            -12 => "p",
-                            -9  => "n",
-                            -6  => "\\mu",
-                            -3  => "m",
-                            -2  => "c",
-                            -1  => "d",
-                            0   => "",
-                            1   => "D",
-                            2   => "h",
-                            3   => "k",
-                            6   => "M",
-                            9   => "G",
-                            12  => "T",
-                            15  => "P",
-                            18  => "E",
-                            21  => "Z",
-                            24  => "Y"
-                           )
-const siunitxprefixes = Dict(
-                             -24 => "\\yocto",
-                             -21 => "\\zepto",
-                             -18 => "\\atto",
-                             -15 => "\\femto",
-                             -12 => "\\pico",
-                             -9  => "\\nano",
-                             -6  => "\\micro",
-                             -3  => "\\milli",
-                             -2  => "\\centi",
-                             -1  => "\\deci",
-                             0   => "",
-                             1   => "\\deka",
-                             2   => "\\hecto",
-                             3   => "\\kilo",
-                             6   => "\\mega",
-                             9   => "\\giga",
-                             12  => "\\tera",
-                             15  => "\\peta",
-                             18  => "\\exa",
-                             21  => "\\zetta",
-                             24  => "\\yotta"
-                            )
+import .Unitful: Unit, FreeUnits, Quantity, power, abbr, name, tens, sortexp, unit
 
-function latexify(q::Union{Unitful.Quantity,Unitful.FreeUnits})
-    return LaTeXString("\$$(latexraw(q))\$")
+const prefixes = begin
+    Dict(
+         (:mathrm,  -24) => "y",
+         (:mathrm,  -21) => "z",
+         (:mathrm,  -18) => "a",
+         (:mathrm,  -15) => "f",
+         (:mathrm,  -12) => "p",
+         (:mathrm,  -9 ) => "n",
+         (:mathrm,  -6 ) => "\\mu",
+         (:mathrm,  -3 ) => "m",
+         (:mathrm,  -2 ) => "c",
+         (:mathrm,  -1 ) => "d",
+         (:mathrm,   0 ) => "",
+         (:mathrm,   1 ) => "D",
+         (:mathrm,   2 ) => "h",
+         (:mathrm,   3 ) => "k",
+         (:mathrm,   6 ) => "M",
+         (:mathrm,   9 ) => "G",
+         (:mathrm,   12) => "T",
+         (:mathrm,   15) => "P",
+         (:mathrm,   18) => "E",
+         (:mathrm,   21) => "Z",
+         (:mathrm,   24) => "Y",
+         (:siunitx, -24) => "\\yocto",
+         (:siunitx, -21) => "\\zepto",
+         (:siunitx, -18) => "\\atto",
+         (:siunitx, -15) => "\\femto",
+         (:siunitx, -12) => "\\pico",
+         (:siunitx, -9 ) => "\\nano",
+         (:siunitx, -6 ) => "\\micro",
+         (:siunitx, -3 ) => "\\milli",
+         (:siunitx, -2 ) => "\\centi",
+         (:siunitx, -1 ) => "\\deci",
+         (:siunitx,  0 ) => "",
+         (:siunitx,  1 ) => "\\deka",
+         (:siunitx,  2 ) => "\\hecto",
+         (:siunitx,  3 ) => "\\kilo",
+         (:siunitx,  6 ) => "\\mega",
+         (:siunitx,  9 ) => "\\giga",
+         (:siunitx,  12) => "\\tera",
+         (:siunitx,  15) => "\\peta",
+         (:siunitx,  18) => "\\exa",
+         (:siunitx,  21) => "\\zetta",
+         (:siunitx,  24) => "\\yotta"
+        )
 end
 
-function latexraw(q::Unitful.Quantity; unitformat=:mathrm, kwargs...)
-    io = IOBuffer()
-    unitformat == :mathrm && print(io,latexraw(q.val;kwargs...),"\\;")
-    unitformat == :siunitx && print(io,"\\SI{",latexraw(q.val;kwargs...),"}{")
-    printunit.(Ref(io),Unitful.sortexp(typeof(Unitful.unit(q)).parameters[1]),Val(unitformat))
-    unitformat == :siunitx && print(io,"}")
-    return String(take!(io))
+
+@latexrecipe function f(p::T;unitformat=:mathrm) where T <: Unit
+    prefix = prefixes[(unitformat, tens(p))]
+    pow = power(p)
+    if unitformat == :mathrm
+        env --> :inline
+        unitname = abbr(p)
+        if pow == 1//1
+            expo = ""
+        else
+            expo = "^{$(latexify(pow;env=:raw))}"
+        end
+        return LaTeXString("\\mathrm{$prefix$unitname}$expo")
+    end
+    env --> :raw
+    unitname = "\\$(lowercase(String(name(p))))"
+    per = pow<0 ? "\\per" : ""
+    pow = abs(pow)
+    expo = pow==1//1 ? "" : "\\tothe{$(latexify(pow;env=:raw))}"
+    return LaTeXString("$per$prefix$unitname$expo")
 end
 
-function latexraw(u::Unitful.FreeUnits; unitformat=:mathrm)
-    io = IOBuffer()
-    unitformat == :siunitx && print(io,"\\si{")
-    printunit.(Ref(io),Unitful.sortexp(typeof(u).parameters[1]),Val(unitformat))
-    unitformat == :siunitx && print(io,"}")
-    return String(take!(io))
+function listunits(::T;unitformat) where T <: FreeUnits
+    return prod(latexify.(sortexp(T.parameters[1]);unitformat,env=:raw))
 end
 
-function printunit(io::IO,u::Unitful.Unit,::Val{:mathrm})
-    print(io,"\\mathrm{")
-    iszero(u.tens) || print(io,mathrmprefixes[u.tens])
-    print(io,Unitful.abbr(u))
-    print(io,"}")
-    u.power==1//1 || print(io,"^{",latexraw(u.power),"}")
+@latexrecipe function f(u::T;unitformat=:mathrm) where T <: FreeUnits
+    if unitformat == :mathrm
+        env --> :inline
+        return LaTeXString(listunits(u;unitformat))
+    end
+    env --> :raw
+    return LaTeXString("\\si{$(listunits(u;unitformat))}")
 end
 
-function printunit(io::IO,u::Unitful.Unit,::Val{:siunitx})
-    u.power<0 && print(io,"\\per")
-    iszero(u.tens) || print(io,siunitxprefixes[u.tens])
-    print(io,"\\",lowercase(String(Unitful.name(u))))
-    abs(u.power)==1//1 || print(io,"\\tothe{",Int64(abs(u.power)),"}")
+@latexrecipe function f(q::T;unitformat=:mathrm) where T <: Quantity
+    if unitformat == :mathrm
+        env --> :inline
+        fmt --> FancyNumberFormatter()
+        return LaTeXString("$(
+                              latexify(q.val,env=:raw)
+                             )\\;$(
+                                   listunits(unit(q);unitformat)
+                                  )")
+    end
+    env --> :raw
+    return LaTeXString("\\SI{$(
+                               latexify(q.val,env=:raw)
+                              )}{$(
+                                   listunits(unit(q);unitformat)
+                                  )}")
 end
--- test/plugins/Unitful.jl
+++ test/plugins/Unitful.jl
@@ -4,7 +4,7 @@ using LaTeXStrings
 u = u"J/kg"
 q = 24.7e9u"Gm/s^2"
 
-@test latexify(u) == "\$\\mathrm{J}\\mathrm{kg}^{-1}\$"
-@test latexify(u,unitformat=:siunitx) == "\$\\si{\\joule\\per\\kilo\\gram}\$"
-@test latexify(q) == "\$2.47e10\\;\\mathrm{Gm}\\mathrm{s}^{-2}\$"
-@test latexify(q,unitformat=:siunitx) == "\$\\SI{2.47e10}{\\giga\\meter\\per\\second\\tothe{2}}\$"
+@test latexify(u) == L"$\mathrm{J}\mathrm{kg}^{-1}$"
+@test latexify(u,unitformat=:siunitx) == LaTeXString("\\si{\\joule\\per\\kilo\\gram}")
+@test latexify(q) == L"$2.47e10\;\mathrm{Gm}\mathrm{s}^{-2}$"
+@test latexify(q,unitformat=:siunitx) == LaTeXString("\\SI{2.47e10}{\\giga\\meter\\per\\second\\tothe{2}}")
