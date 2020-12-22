20fa7e505145f9e737c359270f32218e58c37e11
Niklas Korsbo <niklas@pumas.ai>
2020-12-22 18:27:24 +0100

    Add special :latexmerge handling.

--- src/latexoperation.jl
+++ src/latexoperation.jl
@@ -11,7 +11,15 @@ function latexoperation(ex::Expr, prevOp::AbstractArray; cdot=true, index=:brack
     args = map(i -> typeof(i) ∉ (String, LineNumberNode) ? latexraw(i; kwargs...) : i, ex.args)
 
     # Remove math italics for variables (i.e. words) longer than 2 characters.
-    args = map(i -> (i isa String && all(map(isletter, collect(i))) && length(i) > 2) ? "{\\rm $i}" : i, args)
+    # args = map(i -> (i isa String && all(map(isletter, collect(i))) && length(i) > 2) ? "{\\rm $i}" : i, args)
+
+    if ex.head == :latexifymerge 
+        if all(prevOp .== :none)
+            return Symbol(join(ex.args))
+        else
+            return "$(args[1])\\left( $(join(args[2:end])) \\right)"
+        end
+    end
 
     if op in [:/, :./]
         return "\\frac{$(args[2])}{$(args[3])}"
