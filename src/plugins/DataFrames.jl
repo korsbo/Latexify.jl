c35c65bdbc1b7685ca1f3b4280b955791b7e72fa
Rik Huijzer <rikhuijzer@pm.me>
2021-03-30 18:35:10 +0200

    Avoid deprecated function

--- src/plugins/DataFrames.jl
+++ src/plugins/DataFrames.jl
@@ -11,19 +11,19 @@ get_md_function(args::DataFrames.DataFrame) = mdtable
 
 
 function mdtable(d::DataFrames.DataFrame; kwargs...)
-    body = convert(Matrix, d)
+    body = Matrix(d)
     head = propertynames(d)
     mdtable(body; head=head, kwargs...)
 end
 
 function _latextabular(d::DataFrames.DataFrame; kwargs...)
-    body = convert(Matrix, d)
+    body = Matrix(d)
     head = propertynames(d)
     _latextabular(body; head=head, kwargs...)
 end
 
 function _latexarray(d::DataFrames.DataFrame; kwargs...)
-    body = convert(Matrix, d)
+    body = Matrix(d)
     head = permutedims(propertynames(d))
     result = vcat(head, body)
     _latexarray(result; kwargs...)
