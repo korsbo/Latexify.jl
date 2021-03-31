c4ebdbdd29854275d1f7d52895dec3696c50d251
Niklas Korsbo <niklas.korsbo@gmail.com>
2021-03-31 15:00:12 +0200

    Update latexinline test.

--- test/latexinline_test.jl
+++ test/latexinline_test.jl
@@ -2,7 +2,7 @@ using Latexify
 using LaTeXStrings
 
 test_array = ["x/y * d" :x ; :( (t_sub_sub - x)^(2*p) ) 3//4 ]
-@test latexinline(test_array) == [
+@test latexinline.(test_array) == [
     L"$\frac{x}{y} \cdot d$"                          L"$x$"         ;
     L"$\left( t_{sub\_sub} - x \right)^{2 \cdot p}$"  L"$\frac{3}{4}$"
     ]
