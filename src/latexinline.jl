f50b9f43e53477d8d2a76c1409f12f8bccba6e20
Niklas Korsbo <niklas.korsbo@gmail.com>
2021-03-31 14:40:54 +0200

    Stop auto-broadcasting latexinline.

--- src/latexinline.jl
+++ src/latexinline.jl
@@ -5,7 +5,3 @@ function _latexinline(x; kwargs...)
     COPY_TO_CLIPBOARD && clipboard(latexstr)
     return latexstr
 end
-
-_latexinline(x::AbstractArray; kwargs...) = _latexinline.(x; kwargs...)
-_latexinline(x::Tuple; kwargs...) = [_latexinline.(x; kwargs...)...]
-_latexinline(args...; kwargs...) = _latexinline(args; kwargs...)
