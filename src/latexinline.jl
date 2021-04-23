fae180a443c6b42dc5cf7f03a67ad39a15e5f1db
Niklas Korsbo <niklas.korsbo@gmail.com>
2021-04-23 10:19:32 +0200

    Fix spurious top-level recursion.

--- src/latexinline.jl
+++ src/latexinline.jl
@@ -1,7 +1,7 @@
 latexinline(args...;kwargs...) = process_latexify(args...;kwargs...,env=:inline)
 
 function _latexinline(x; kwargs...)
-    latexstr = latexstring( latexify(x; kwargs...,env=:raw) )
+    latexstr = latexstring( process_latexify(x; kwargs...,env=:raw) )
     COPY_TO_CLIPBOARD && clipboard(latexstr)
     return latexstr
 end
