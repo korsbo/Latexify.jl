16f2524c5995c65a16e67ac453da29f798b206d9
David MacMahon <davidm@astro.berkeley.edu>
2021-12-23 20:28:35 +0000

    Drop leading zeros in scientific notation exponent
    This changes `float_regex` such that leading zeroes on scientific
    notation exponents are dropped for both positive and negative
    exponents, whereas previously they were only dropped for positive
    exponents.  Most of the named groups in the regex are unchanged, but the
    `'exp'` group is gone and a new `'mag_exp'` group is created.  Instead
    of using `s"\g<exp>"` to access the matched exponent, one should use
    `s"\g<sign_exp>\g<mag_exp>"`.  All uses of `s"\g<exp>"` in this package
    have been replaced accordingly.  Tests have been updated accordingly as
    well (and pass).
    
    Because this changes the named groups, I think it may qualify as a
    breaking change even though `float_regex` is not exported.

--- src/numberformatters.jl
+++ src/numberformatters.jl
@@ -1,6 +1,6 @@
 abstract type AbstractNumberFormatter end
 
-const float_regex = r"(?'mantissa'(?'before_dp'(?'sign'-?)(?'before_dp_nosign'\d+))(\.(?'after_dp'\d+))?)(?'e_or_E'e)(?'raw_exp'\+?0*(?'exp'(?'sign_exp'-?)\d+))"i
+const float_regex = r"(?'mantissa'(?'before_dp'(?'sign'-?)(?'before_dp_nosign'\d+))(\.(?'after_dp'\d+))?)(?'e_or_E'e)(?'raw_exp'(?'sign_exp'-?)\+?0*(?'mag_exp'\d+))"i
 
 struct PlainNumberFormatter <: AbstractNumberFormatter end
 
@@ -29,7 +29,7 @@ end
 (f::StyledNumberFormatter)(x) = string(x)
 function (f::StyledNumberFormatter)(x::AbstractFloat)
     s = @eval @sprintf $(f.fmt) $x
-    return replace(s, float_regex => s"\g<mantissa> \\mathrm{\g<e_or_E>} \g<exp>")
+    return replace(s, float_regex => s"\g<mantissa> \\mathrm{\g<e_or_E>} \g<sign_exp>\g<mag_exp>")
 end
 
 (f::StyledNumberFormatter)(x::Unsigned) = "\\mathtt{0x$(string(x; base=16, pad=2sizeof(x)))}"
@@ -40,13 +40,13 @@ struct FancyNumberFormatter <: AbstractNumberFormatter
     exponent_format::SubstitutionString
 
     function FancyNumberFormatter(fmt::String="%.4g",
-                                  exponent_format::SubstitutionString=s"\g<mantissa> \\cdot 10^{\g<exp>}")
+                                  exponent_format::SubstitutionString=s"\g<mantissa> \\cdot 10^{\g<sign_exp>\g<mag_exp>}")
         return new(fmt, exponent_format)
     end
 end
 
 function FancyNumberFormatter(fmt::String, mult_symbol)
-    return FancyNumberFormatter(fmt, SubstitutionString("\\g<mantissa> $(escape_string(mult_symbol)) 10^{\\g<exp>}"))
+    return FancyNumberFormatter(fmt, SubstitutionString("\\g<mantissa> $(escape_string(mult_symbol)) 10^{\\g<sign_exp>\\g<mag_exp>}"))
 end
 function FancyNumberFormatter(significant_digits, mult_symbol="\\cdot")
     return FancyNumberFormatter("%.$(significant_digits)g", mult_symbol)
--- test/numberformatters_test.jl
+++ test/numberformatters_test.jl
@@ -2,7 +2,7 @@ using Latexify
 using Test
 import Latexify: PlainNumberFormatter, PrintfNumberFormatter
 
-@test FancyNumberFormatter() == FancyNumberFormatter(4) == FancyNumberFormatter("%.4g", "\\cdot") == FancyNumberFormatter("%.4g", s"\g<mantissa> \\cdot 10^{\g<exp>}")
+@test FancyNumberFormatter() == FancyNumberFormatter(4) == FancyNumberFormatter("%.4g", "\\cdot") == FancyNumberFormatter("%.4g", s"\g<mantissa> \\cdot 10^{\g<sign_exp>\g<mag_exp>}")
 
 x = -23.4728979e7
 
@@ -10,12 +10,12 @@ x = -23.4728979e7
 @test PrintfNumberFormatter("%.4g")(x) == "-2.347e+08"
 @test StyledNumberFormatter()(x) == "-2.347 \\mathrm{e} 8"
 @test FancyNumberFormatter()(x) == "-2.347 \\cdot 10^{8}"
-@test FancyNumberFormatter("%.5E", s"\g<mantissa>,\g<before_dp>,\g<sign>,\g<before_dp_nosign>,\g<after_dp>,\g<e_or_E>,\g<raw_exp>,\g<exp>,\g<sign_exp>")(x) == "-2.34729,-2,-,2,34729,E,+08,8,"
+@test FancyNumberFormatter("%.5E", s"\g<mantissa>,\g<before_dp>,\g<sign>,\g<before_dp_nosign>,\g<after_dp>,\g<e_or_E>,\g<raw_exp>,\g<sign_exp>,\g<mag_exp>")(x) == "-2.34729,-2,-,2,34729,E,+08,,8"
 
 
 xne = -23.4728979e-7
 
-@test FancyNumberFormatter("%.5E", s"\g<mantissa>,\g<before_dp>,\g<sign>,\g<before_dp_nosign>,\g<after_dp>,\g<e_or_E>,\g<raw_exp>,\g<exp>,\g<sign_exp>")(xne) == "-2.34729,-2,-,2,34729,E,-06,-6,-"
+@test FancyNumberFormatter("%.5E", s"\g<mantissa>,\g<before_dp>,\g<sign>,\g<before_dp_nosign>,\g<after_dp>,\g<e_or_E>,\g<raw_exp>,\g<sign_exp>,\g<mag_exp>")(xne) == "-2.34729,-2,-,2,34729,E,-06,-,6"
 
 y = 0xf43
 
