9082e18b88595f58e3d9e942e3dc02e9c97647c0
David MacMahon <davidm@astro.berkeley.edu>
2022-01-12 15:43:45 -0800

    Put StyledNumberFormatter exponent in gull wings
    Putting the exponent in gull wings treats the `-` of a negative exponent
    as an unary minus sign, which gets less spaced out and looks better in
    this context than a non-unary minus sign.

--- test/numberformatters_test.jl
+++ test/numberformatters_test.jl
@@ -8,7 +8,7 @@ x = -23.4728979e7
 
 @test PlainNumberFormatter()(x) == "-2.34728979e8"
 @test PrintfNumberFormatter("%.4g")(x) == "-2.347e+08"
-@test StyledNumberFormatter()(x) == "-2.347 \\mathrm{e} 8"
+@test StyledNumberFormatter()(x) == "-2.347 \\mathrm{e}{8}"
 @test FancyNumberFormatter()(x) == "-2.347 \\cdot 10^{8}"
 @test FancyNumberFormatter("%.5E", s"\g<mantissa>,\g<before_dp>,\g<sign>,\g<before_dp_nosign>,\g<after_dp>,\g<e_or_E>,\g<raw_exp>,\g<sign_exp>,\g<mag_exp>")(x) == "-2.34729,-2,-,2,34729,E,+08,,8"
 
