19897cda9dc4a1b26318b07e12d090a26579cde4
t-bltg <tf.bltg@gmail.com>
2022-08-11 00:02:00 +0200

    do not wrap in `mathrm` when non ascii function name

--- test/unicode2latex.jl
+++ test/unicode2latex.jl
@@ -37,7 +37,7 @@ s = 'I' * Char(0x308) * 'Z' * Char(0x304) * 'E' * Char(0x306)
 @test latexify(s; parse=false).s == raw"$\textnormal{\\\"{I}}\textnormal{\={Z}}\textnormal{\u{E}}$"
 
 @test latexify(:(iħ * (∂Ψ(𝐫, t) / ∂t) = -ħ^2 / 2m * ΔΨ(𝐫, t) + V * Ψ(𝐫, t))) ==
-  L"$i\hslash \cdot \frac{\mathrm{\partial\Psi}\left( \mathbf{r}, t \right)}{{\partial}t} = \frac{ - \hslash^{2}}{2 \cdot m} \cdot \mathrm{\Delta\Psi}\left( \mathbf{r}, t \right) + V \cdot \Psi\left( \mathbf{r}, t \right)$"
+  L"$i\hslash \cdot \frac{\partial\Psi\left( \mathbf{r}, t \right)}{{\partial}t} = \frac{ - \hslash^{2}}{2 \cdot m} \cdot \Delta\Psi\left( \mathbf{r}, t \right) + V \cdot \Psi\left( \mathbf{r}, t \right)$"
 
 if Sys.islinux()
   mktempdir() do dn
