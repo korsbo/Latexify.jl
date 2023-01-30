c0c0dbeee67c59f933246d06342626376f30a971
David Gustavsson <david.e.gustavsson@gmail.com>
2023-01-30 17:11:48 +0100

    Recipes benchmark

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -6,10 +6,23 @@ const SUITE = BenchmarkGroup()
 chars = vcat(
              'A':'Z',
              'a':'z',
-             'Α':'Ρ', 'Σ':'Ω',
+             'Α':'Ρ', # skip unprintable char (no \Varsigma)
+             'Σ':'Ω',
              'α':'ω',
              '𝕒':'𝕫',
              '𝐴':'𝑍',
             )
 
 SUITE["unicode"] = @benchmarkable latexify(string(c)) setup = (c = rand(chars))
+
+struct AType
+    x
+end
+struct BType
+    a
+end
+
+@latexrecipe f(a::AType) = :($(a.x) + 1)
+@latexrecipe f(b::BType) = :($(b.a)/2)
+
+SUITE["user types"] = @benchmarkable latexify(BType(AType(x))) setup = (x=rand())
