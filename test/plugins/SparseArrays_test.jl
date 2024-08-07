d8427e7228d72103d6685f88ba1bf010f4f863a9
David Gustavsson <david.e.gustavsson@gmail.com>
2024-08-07 22:58:41 +0200

    aand make windows happy

--- test/plugins/SparseArrays_test.jl
+++ test/plugins/SparseArrays_test.jl
@@ -1,10 +1,11 @@
 using SparseArrays
 
 x = sparse([1,2,3],[1,3,2],[0,1,2])
-@test latexraw(x) == raw"""\left[
+@test latexraw(x) == replace(raw"""\left[
 \begin{array}{ccc}
 0 & 0 & 0 \\
 0 & 0 & 1 \\
 0 & 2 & 0 \\
 \end{array}
-\right]"""
+\right]""", "\r\n"=>"\n")
+
