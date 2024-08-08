d9ead6dce329f857709342eca1dd89721d01ed81
David Gustavsson <david.e.gustavsson@gmail.com>
2024-08-08 17:19:37 +0200

    Oops

--- test/visualregression_tests.jl
+++ test/visualregression_tests.jl
@@ -5,7 +5,7 @@ end
 
 function latexify_VRT(filename, args...; kwargs...)
     testfun(fname) = render(latexify(args...; kwargs...), MIME"image/png"(); name=replace(fname, r".png$"=>""), transparent=false)
-    @visualtest testfun joinpath("visualreferences", filename) $(isinteractive())
+    @visualtest testfun joinpath("visualreferences", filename) isinteractive()
 end
 
 latexify_VRT("simpleequation.png", :(x^2-2y_a*exp(3)∈[1,2,3]); cdot=false)
