9e31431148d7c8ad5b898fba7336836b1591a861
David Gustavsson <david.e.gustavsson@gmail.com>
2024-08-09 09:46:49 +0200

    Add another test

--- test/visualregression_tests.jl
+++ test/visualregression_tests.jl
@@ -12,3 +12,10 @@ end
 latexify_VRT("simpleequation.png", :(x^2-2y_a*exp(3)∈[1,2,3]); cdot=false)
 latexify_VRT("table.png", hcat([1,2,3], [4,5,6], [7;8;9]); env=:table, head=["a" "b" "c"])
 
+struct Ket{T}
+    x::T
+end
+@latexrecipe function f(x::Ket)
+    return Expr(:latexifymerge, "\\left|", x.x, "\\right>")
+end
+latexify_VRT("ket.png", :($(Ket(:a)) + $(Ket(:b))))
