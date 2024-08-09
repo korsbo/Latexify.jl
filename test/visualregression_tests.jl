46a3be08543f638857cd6d3a2ff54bac445f0f21
David Gustavsson <david.e.gustavsson@gmail.com>
2024-08-09 09:24:34 +0200

    interactive->non-ci

--- test/visualregression_tests.jl
+++ test/visualregression_tests.jl
@@ -1,13 +1,14 @@
 using Latexify, VisualRegressionTests
-if isinteractive()
+is_ci() = parse(Bool, get(ENV, "CI", "false"))
+if ~is_ci()
     using Gtk
 end
 
 function latexify_VRT(filename, args...; kwargs...)
     testfun(fname) = render(latexify(args...; kwargs...), MIME"image/png"(); name=replace(fname, r".png$"=>""), transparent=false)
-    @visualtest testfun joinpath("visualreferences", filename) isinteractive()
+    @visualtest testfun joinpath("visualreferences", filename) ~is_ci()
 end
 
 latexify_VRT("simpleequation.png", :(x^2-2y_a*exp(3)∈[1,2,3]); cdot=false)
-latexify_VRT("table.png", [1;2;3;;4;5;6;;7;8;9]; env=:table, booktabs=true, head=["a" "b" "c"])
+latexify_VRT("table.png", [1;2;3;;4;5;6;;7;8;9]; env=:table, head=["a" "b" "c"])
 
