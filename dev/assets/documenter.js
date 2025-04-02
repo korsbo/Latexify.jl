b2f7f7390e0c647c33652fc1021f4c9e9a5040e9
Documenter.jl <documenter@juliadocs.github.io>
2025-04-02 07:54:47 +0000

    build based on d930a7b

--- dev/assets/documenter.js
+++ dev/assets/documenter.js
@@ -723,6 +723,9 @@ function runSearchMainCode() {
   };
 
   $(document).on("click", ".search-filter", function () {
+    let search_input = $(".documenter-search-input");
+    let cursor_position = search_input[0].selectionStart;
+
     if ($(this).hasClass("search-filter-selected")) {
       selected_filter = "";
     } else {
@@ -731,6 +734,9 @@ function runSearchMainCode() {
 
     // This updates search results and toggles classes for UI:
     update_search();
+
+    search_input.focus();
+    search_input.setSelectionRange(cursor_position, cursor_position);
   });
 
   /**
