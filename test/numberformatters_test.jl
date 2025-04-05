41e08826a4796bcb43e49f17418829aa6d7de3cf
David Gustavsson <david.e.gustavsson@gmail.com>
2025-04-05 09:05:42 +0200

    Some more tests

--- test/numberformatters_test.jl
+++ test/numberformatters_test.jl
@@ -26,3 +26,26 @@ y = 0xf43
 @test SiunitxNumberFormatter(version=2)(x) == "\\si{-2.34728979e8}"
 @test SiunitxNumberFormatter(format_options="something")(x) == "\\num[something]{-2.34728979e8}"
 @test SiunitxNumberFormatter(format_options="[something]")(x) == "\\num[something]{-2.34728979e8}"
+
+@test SiunitxNumberFormatter()([1,2,4]) == "\\numlist{1;2;4}"
+@test SiunitxNumberFormatter()(1:4) == "\\numrange{1}{4}"
+
+@test Latexify.siunitxcommand(:number, 2) == "si"
+@test Latexify.siunitxcommand(:quantity, 2) == "SI"
+@test Latexify.siunitxcommand(:numberrange, 2) == "sirange"
+@test Latexify.siunitxcommand(:quantityrange, 2) == "SIrange"
+@test Latexify.siunitxcommand(:numberlist, 2) == "silist"
+@test Latexify.siunitxcommand(:quantitylist, 2) == "SIlist"
+@test Latexify.siunitxcommand(:numberproduct, 2) == "siproduct"
+@test Latexify.siunitxcommand(:quantityproduct, 2) == "SIproduct"
+
+@test Latexify.siunitxcommand(:number, 3) == "num"
+@test Latexify.siunitxcommand(:quantity, 3) == "qty"
+@test Latexify.siunitxcommand(:numberrange, 3) == "numrange"
+@test Latexify.siunitxcommand(:quantityrange, 3) == "qtyrange"
+@test Latexify.siunitxcommand(:numberlist, 3) == "numlist"
+@test Latexify.siunitxcommand(:quantitylist, 3) == "qtylist"
+@test Latexify.siunitxcommand(:numberproduct, 3) == "numproduct"
+@test Latexify.siunitxcommand(:quantityproduct, 3) == "qtyproduct"
+
+@test_throws ArgumentError Latexify.siunitxcommand(:nonsense, 1)
